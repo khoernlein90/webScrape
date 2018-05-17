6af6a38f0e0ed8ea0288f1cbdcb86e4d83035141
Kyle Hoernlein <khoernlein90@gmail.com>
2018-05-17 11:24:15 -0400

    more refactor

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -10,19 +10,15 @@ function getArticles() {
                 redditHREF = data[i].link
             }
             const card = $("<div>").addClass("card")
-            // const cardHeader = $("<div>").addClass("main-card-header")
             const headerText = $("<div>").text(data[i].title).addClass("title")
             const saveButton = $("<button>").text("SAVE ARTICLE").addClass("save-button").data(data[i])
             const img = $("<img>").attr("src", data[i].img)
-            // cardHeader.append(headerText, saveButton)
-            // const cardBody = $("<div>").addClass("main-card-body");
             const likes = $("<div>").addClass("likes").html(`Upvotes:<p>${data[i].likes}</p>`)
             const a = $("<a>").attr({
                 class: "card-text",
                 href: redditHREF,
                 target: "_blank"
             }).text(data[i].link)
-            // cardBody.append(likes, a)
             card.append(headerText, saveButton, likes, img, a)
             $("#articles").append(card)
         }
@@ -38,24 +34,6 @@ $("#scrapeButton").on("click", function () {
             $("#articles").empty()
             getArticles()
             location.reload()
-            // $.getJSON("/api/articles", function (data) {
-            //     for (var i = 0; i < data.length; i++) {
-            //         const card = $("<div>").addClass("card")
-            //         const cardHeader = $("<div>").addClass("card-header bg-primary text-white")
-            //         const headerText = $("<div>").addClass("col-md-10").html(`<h5>${data[i].title}</h5>`)
-            //         const saveButton = $("<button>").text("SAVE ARTICLE").addClass("save-button").data(data[i])
-            //         cardHeader.append(headerText, saveButton)
-            //         const cardBody = $("<div>").addClass("card-body");
-            //         const a = $("<a>").attr({
-            //             class: "card-text",
-            //             href: data[i].link,
-            //             target: "_blank"
-            //         }).text(data[i].link)
-            //         cardBody.append(a)
-            //         card.append(cardHeader, cardBody)
-            //         $("#articles").append(card)
-            //     }
-            // });
         })
 })
 
